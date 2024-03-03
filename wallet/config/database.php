caececd62b229d196f7ec25c9bce5884cf183029
IvTema <duft.soft@gmail.com>
2024-03-03 21:31:50 +0100

    redis container

--- wallet/config/database.php
+++ wallet/config/database.php
@@ -121,7 +121,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -147,5 +147,25 @@ return [
         ],
 
     ],
+
+    // 'redis' => [
+ 
+        // 'client' => env('REDIS_CLIENT', 'phpredis'),
+     
+        // 'default' => [
+        //     'host' => env('REDIS_HOST', '127.0.0.1'),
+        //     'password' => env('REDIS_PASSWORD'),
+        //     'port' => env('REDIS_PORT', 6379),
+        //     'database' => env('REDIS_DB', 0),
+        // ],
+     
+        // 'cache' => [
+        //     'host' => env('REDIS_HOST', '127.0.0.1'),
+        //     'password' => env('REDIS_PASSWORD'),
+        //     'port' => env('REDIS_PORT', 6379),
+        //     'database' => env('REDIS_CACHE_DB', 1),
+    //     ],
+     
+    // ],
 
 ];
